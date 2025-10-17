ff4918827ea93fcea08c6f913be57d262af321bf
Choko <yepimchoko@gmail.com>
2025-10-17 20:32:49 +0200

    update verif

--- src/lib/verification.ts
+++ src/lib/verification.ts
@@ -1,26 +1,26 @@
-export function validateArtistAlbumMbid(options: { artist?: string; album?: string; mbid?: string }): void {
+export function validateArtistAlbumMbid(options: { artist?: string | undefined; album?: string | undefined; mbid?: string | undefined }): void {
   validateArtistMbid({ mbid: options.mbid, artist: options.artist });
   validateAlbumMbid({ mbid: options.mbid, album: options.album });
 }
 
-export function validateArtistTrackMbid(options: { track?: string; artist?: string; mbid?: string }): void {
+export function validateArtistTrackMbid(options: { track?: string | undefined; artist?: string | undefined; mbid?: string | undefined }): void {
   validateArtistMbid({ mbid: options.mbid, artist: options.artist });
   validateTrackMbid({ mbid: options.mbid, track: options.track });
 }
 
-export function validateArtistMbid(options: { artist?: string; mbid?: string }): void {
+export function validateArtistMbid(options: { artist?: string | undefined; mbid?: string | undefined }): void {
   if (!options.artist && !options.mbid) {
     throw new Error("Artist name or MusicBrainz artist ID is required.");
   }
 }
 
-export function validateAlbumMbid(options: { album?: string; mbid?: string }): void {
+export function validateAlbumMbid(options: { album?: string | undefined; mbid?: string | undefined }): void {
   if (!options.album && !options.mbid) {
     throw new Error("Album name or MusicBrainz album ID is required.");
   }
 }
 
-export function validateTrackMbid(options: { track?: string; mbid?: string }): void {
+export function validateTrackMbid(options: { track?: string | undefined; mbid?: string | undefined }): void {
   if (!options.track && !options.mbid) {
     throw new Error("Track name or MusicBrainz album ID is required.");
   }
